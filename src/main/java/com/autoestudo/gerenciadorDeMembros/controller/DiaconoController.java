36e813e2709f32c7cb643c76ed9fd3ccf2b999ed
Henok Teixeira <henokteixeira@gmail.com>
2024-03-12 08:36:48 -0300

    feat: add cors configuration

--- src/main/java/com/autoestudo/gerenciadorDeMembros/controller/DiaconoController.java
+++ src/main/java/com/autoestudo/gerenciadorDeMembros/controller/DiaconoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/diaconos")
 public class DiaconoController {
     private final DiaconoRepository diaconoRepository;
--- src/main/java/com/autoestudo/gerenciadorDeMembros/controller/MembroController.java
+++ src/main/java/com/autoestudo/gerenciadorDeMembros/controller/MembroController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/membros")
 public class MembroController {
     private final MembroRepository membroRepository;
