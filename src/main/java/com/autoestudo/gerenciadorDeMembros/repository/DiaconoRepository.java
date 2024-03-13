8ce94bced7158850d48ed906ab1ab48e9f48d1d4
Henok Teixeira <henokteixeira@gmail.com>
2024-03-13 10:03:12 -0300

    refactor: remove unnecessary import

--- src/main/java/com/autoestudo/gerenciadorDeMembros/repository/DiaconoRepository.java
+++ src/main/java/com/autoestudo/gerenciadorDeMembros/repository/DiaconoRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface DiaconoRepository extends CrudRepository<MembroEntity, Long> {
     public <T> Page<T> findByDiaconoResponsavelNull(Pageable pageable, Class<T> type);
     public <T> Page<T> findByDiaconoResponsavelId(Pageable pageable, Long id, Class<T> type);
