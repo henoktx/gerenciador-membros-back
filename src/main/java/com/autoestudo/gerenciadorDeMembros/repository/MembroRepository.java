8c3a3641d69603c61cb936ade21aab443ae5c457
Henok Teixeira <henokteixeira@gmail.com>
2024-03-13 10:05:36 -0300

    feat: update get by nome, add find by id and remove findBy

--- src/main/java/com/autoestudo/gerenciadorDeMembros/repository/MembroRepository.java
+++ src/main/java/com/autoestudo/gerenciadorDeMembros/repository/MembroRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MembroRepository extends CrudRepository<MembroEntity, Long> {
-    public <T> Page<T> findByNomeCompletoContains(Pageable pageable, String nome, Class<T> type);
-    public <T> Page<T> findBy(Pageable pageable, Class<T> type);
+    public <T> Page<T> findByNomeCompletoContainsIgnoreCase(Pageable pageable, String nome, Class<T> type);
+    public <T> Optional<T> findById(long id, Class<T> type);
 }
